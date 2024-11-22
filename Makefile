316fe544eb43e23813f41f75d068b659168ea062
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2024-11-22 21:50:04 +0100

    init

---
+++ Makefile
@@ -0,0 +1,27 @@
+DAY=$$(date +"%d")
+YEAR=$$(date +"%Y")
+DAY_SHORT=$$(echo $(DAY) | sed 's/^0*//')
+TOKEN=$$(cat cookie)
+
+.PHONY: csharp
+csharp:
+	echo $(DAY)
+	cp -R charp $(DAY)
+	mv $(DAY)/tmp.csproj $(DAY)/aoc$(DAY).csproj
+	rm -rf $(DAY)/bin
+	rm -rf $(DAY)/obj
+	mv $(DAY)/Program.cs $(DAY)/Day$(DAY).cs
+	curl --cookie session=$(TOKEN) https://adventofcode.com/$(YEAR)/day/$(DAY_SHORT)/input > $(DAY)/input.txt
+	dotnet sln add $(DAY)/aoc$(DAY).csproj
+
+.PHONY: kotlin
+kotlin:
+	echo $(DAY)
+	cp -R kotlin $(DAY)
+	mv $(DAY)/tmp.iml $(DAY)/aoc$(DAY).iml
+	sed -i "s/tmp/aoc$(DAY)/g" ./$(DAY)/.idea/modules.xml
+	curl --cookie session=$(TOKEN) https://adventofcode.com/$(YEAR)/day/$(DAY_SHORT)/input > $(DAY)/input.txt
+
+.PHONY: token
+token:
+	echo $(TOKEN)
---
+++ utils/Lib.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+namespace utils;
+
+public class Dict<TKey, TValue> : Dictionary<TKey, TValue> where TKey : notnull
+{
+    public Dict() : base() { }
+
+    public Dict(Dictionary<TKey, TValue> d) : base(d) { }
+
+    public override string ToString()
+    {
+        return $"{{{string.Join(", ", this.Select(x => $"{x.Key}={x.Value}"))}}}";
+    }
+}
+
+public class Stck<TValue> : Stack<TValue> where TValue : notnull
+{
+    public Stck() : base() { }
+    public Stck(IEnumerable<TValue> values) : base(values) { }
+
+    public override string ToString()
+    {
+        return $"[{string.Join(",", this)}]";
+    }
+}
+
+public class Lst<TValue> : List<TValue> where TValue : notnull
+{
+    public Lst() : base() { }
+    public Lst(IEnumerable<TValue> collection) : base(collection) { }
+
+    public Lst(int capacity) : base(capacity)
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"[{string.Join(", ", this)}]";
+    }
+}
+
+public static class Extensions
+{
+    public static T Also<T>(this T self, Action<T> action)
+    {
+        action(self);
+        return self;
+    }
+
+    public static K Let<T, K>(this T self, Func<T, K> func)
+    {
+        return func(self);
+    }
+
+    public static Lst<T> ToLst<T>(this IEnumerable<T> self) where T : notnull
+    {
+        return new(self);
+    }
+
+    public static Dict<T, K> ToDict<T, K>(this Dictionary<T, K> self) where T : notnull
+    {
+        return new(self);
+    }
+}
+
+
+public static class ArrayUtils
+{
+    // Array of all the 8 directions
+    public static readonly (int, int)[] frame = new (int, int)[]
+    {
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    };
+
+    public static readonly (int, int)[] adjecent = new (int, int)[]
+    {
+        (0, -1), (0, 1),
+        (-1, 0), (1, 0)
+    };
+}
+
