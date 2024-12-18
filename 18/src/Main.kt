c73c618a94241b11d04a2b1be6b52bce0c7c30a0
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2024-12-18 19:14:19 +0100

    18: refactor

--- 18/src/Main.kt
+++ 18/src/Main.kt
@@ -5,104 +5,79 @@ fun main(args: Array<String>) {
     val input = parse(args.firstOrNull() ?: "input.txt")
 
     println("Part01: ${part01(input, Coord(71, 71), 1024)}")
-    println("Part02: ${part02(input, Coord(71, 71), 1024)}")
+    println("Part02: ${part02(input, Coord(71, 71), 1024) }")
 }
 
 fun part01(input: Array<Coord>, size: Coord, byteSize: Int): Long {
 
     val start = Coord(0, 0)
-    val end = Coord(size.x-1, size.y-1)
+    val end = Coord(size.x - 1, size.y - 1)
 
-    val m = mutableSetOf<Coord>()
-    input.take(byteSize).forEach { m.add(it) }
+    val memorySpace = mutableSetOf<Coord>()
+    input.take(byteSize).forEach { memorySpace.add(it) }
 
-    data class Node(val pos: Coord, val step: Long)
-    val visited = mutableSetOf<Coord>()
-    val queue = PriorityQueue<Node>({a, b -> a.step.compareTo(b.step)})
-    queue.add(Node(start, 0L))
-    while(queue.isNotEmpty()) {
-        val node = queue.poll()
-        if(visited.contains(node.pos)) {
-            continue
-        }
-        visited.add(node.pos)
-
-        if(node.pos == end) {
-            return node.step
-        }
-
-        node.pos.neighbours()
-            .filter { !visited.contains(it) && !m.contains(it) && size.isInBounds(it) }
-            .forEach { neighbour ->
-                queue.add(Node(neighbour, node.step + 1))
-            }
-    }
-    return -1L
+    return djikstra(start, end, size, memorySpace)?.step ?: -1L
 }
 
 fun part02(input: Array<Coord>, size: Coord, byteSize: Int): String {
 
     val start = Coord(0, 0)
-    val end = Coord(size.x-1, size.y-1)
+    val end = Coord(size.x - 1, size.y - 1)
 
-    val m = mutableSetOf<Coord>()
-    input.take(byteSize).forEach { m.add(it) }
+    val memorySpace = mutableSetOf<Coord>()
+    input.take(byteSize).forEach { memorySpace.add(it) }
 
-    fun foundPath(): Boolean {
-        data class Node(val pos: Coord, val step: Long)
-        val visited = mutableSetOf<Coord>()
-        val queue = PriorityQueue<Node>({a, b -> a.step.compareTo(b.step)})
-        queue.add(Node(start, 0L))
-        while(queue.isNotEmpty()) {
-            val node = queue.poll()
-            if(visited.contains(node.pos)) {
-                continue
+    return input.drop(byteSize)
+            .first {
+                memorySpace.add(it)
+                djikstra(start, end, size, memorySpace) == null
             }
-            visited.add(node.pos)
-
-            if(node.pos == end) {
-                return true
-            }
-
-            node.pos.neighbours()
-                .filter { !visited.contains(it) && !m.contains(it) && size.isInBounds(it) }
-                .forEach { neighbour ->
-                    queue.add(Node(neighbour, node.step + 1))
-                }
-        }
-        return false
-    }
-
-    val s = input.drop(byteSize).first {
-        m.add(it)
-        !foundPath()
-    }
-    return "${s.y},${s.x}"
+            .let { "${it.y},${it.x}" }
 }
 
+data class Node(val pos: Coord, val step: Long)
+
+fun djikstra(start: Coord, end: Coord, size: Coord, memorySpace: MutableSet<Coord>): Node? {
+    val visited = mutableSetOf<Coord>()
+    val queue = PriorityQueue<Node>({ a, b -> a.step.compareTo(b.step) })
+    queue.add(Node(start, 0L))
+    while (queue.isNotEmpty()) {
+        val node = queue.poll()
+        if (visited.contains(node.pos)) {
+            continue
+        }
+        visited.add(node.pos)
+
+        if (node.pos == end) {
+            return node
+        }
+
+        node.pos
+                .neighbours()
+                .filter {
+                    !visited.contains(it) && !memorySpace.contains(it) && size.isInBounds(it)
+                }
+                .forEach { neighbour -> queue.add(Node(neighbour, node.step + 1)) }
+    }
+    return null
+}
 
 fun parse(fileName: String): Array<Coord> {
-    return File(fileName).readLines()
-        .map { it.split(",") }
-        .map { Coord(it.last().toInt(), it.first().toInt()) }
-        .toTypedArray()
+    return File(fileName)
+            .readLines()
+            .map { it.split(",") }
+            .map { Coord(it.last().toInt(), it.first().toInt()) }
+            .toTypedArray()
 }
-
 
 data class Coord(val x: Int, val y: Int) {
     operator fun plus(o: Coord): Coord = Coord(this.x + o.x, this.y + o.y)
-    operator fun minus(o: Coord): Coord = Coord(this.x - o.x, this.y - o.y)
 
     fun neighbours(): List<Coord> = DIRECTIONS.map { this + it }
 
     fun isInBounds(o: Coord): Boolean = o.x >= 0 && o.x < this.x && o.y >= 0 && o.y < this.y
 
     companion object {
-        val DIRECTIONS = mutableListOf(
-            Coord(-1, 0),
-            Coord(1, 0),
-            Coord(0, 1),
-            Coord(0, -1)
-        )
+        val DIRECTIONS = mutableListOf(Coord(-1, 0), Coord(1, 0), Coord(0, 1), Coord(0, -1))
     }
 }
